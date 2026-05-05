ecc6f4a2d1ecfa77f3ce870a16dae9efb9660f93
juancamilovalbuena3-debug <juancamilovalbuena3@gmail.com>
2026-05-05 18:19:58 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
+php artisan config:clear
+php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force
-php artisan session:table 2>/dev/null || true
 php artisan migrate --force
 
 apache2-foreground
