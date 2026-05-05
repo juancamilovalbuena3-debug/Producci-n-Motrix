fb50aad725a6b7c9c6448ca886ffb4579277b487
juancamilovalbuena3-debug <juancamilovalbuena3@gmail.com>
2026-05-05 18:38:47 -0500

    Remove caching commands and add clear commands

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,8 @@ set -e
 
 php artisan config:clear
 php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+php artisan route:clear
+php artisan view:clear
 php artisan migrate --force
 
 apache2-foreground
