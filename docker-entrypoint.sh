5392ac2a57fc13ca25c35e55bc69d845413fa7ec
juancamilovalbuena3-debug <juancamilovalbuena3@gmail.com>
2026-05-05 16:15:18 -0500

    Enable Laravel log output in Docker entrypoint
    Add logging for Laravel by tailing the log file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,8 @@ php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
+# Mostrar logs de Laravel en stdout
+touch /var/www/html/storage/logs/laravel.log
+tail -f /var/www/html/storage/logs/laravel.log &
+
 apache2-foreground
